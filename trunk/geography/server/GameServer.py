bf54adcf835781c587b25a744c9709bfd6ec986e
craigswank <craigswank@5e592bd1-0741-0410-9f10-5533d471c3dc>
2007-12-20 06:42:50 +0000

    Changed stuff

--- trunk/geography/server/GameServer.py
+++ trunk/geography/server/GameServer.py
@@ -2,22 +2,22 @@ from twisted.spread import pb
 from GameData import GameData
 
 class GamePerspective(pb.Root):
-    def __init__(self, gameScores):
-        self.gameScores = gameScores
+    def __init__(self, gameData):
+        self.gameData = gameData
         
     def remote_getScores(self):
-        return self.gameScores.getScores()
+        return self.gameData.getData()
     
-    def remote_getLandmarks(self):
-        return self.gameScores.getLandmarks()
+    def remote_getLandmarks(self, difficulty):
+        return self.gameData.getLandmarks(difficulty)
     
     def remote_addScore(self, score):
-        self.gameScores.addScore(score)
+        self.gameData.addScore(score)
 
 if __name__ == "__main__":
     import sys
     from twisted.internet import reactor
-    gameScores = GameScores()
-    gamePerspective = GamePerspective(gameScores)
-    reactor.listenTCP(8789, pb.PBServerFactory(gamePerspective))
+    gameData = GameData()
+    gamePerspective = GamePerspective(gameData)
+    reactor.listenTCP(8387, pb.PBServerFactory(gamePerspective))
     reactor.run()
